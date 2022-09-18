74cbb38b5926cd98c8642da4c457fa6e37960ff1
Asimakis Argyropoulos <makis31179@hotmail.com>
2022-09-18 21:37:13 +0300

    Working on Exercises handler

--- FirstWindow.py
+++ FirstWindow.py
@@ -1,67 +1,9 @@
 #!/usr/bin/env python3
 from PyQt5.QtWidgets import QMessageBox, QPushButton, QWidget, QApplication, QVBoxLayout
 
+from app import MultipleChoiceLayout,MultipleChoiceQuestion
 
-def Lessons():
-    """Event handler for the button Μαθήματα.
-
-    Args:
-        None
-
-    Returns:
-        Nothing
-
-    """
-    alert = QMessageBox()
-    alert.setText('You clicked Μαθήματα!')
-    alert.exec()
-
-
-def Exercises():
-    """Event handler for the button Ασκήσεις.
-
-    Args:
-        None
-
-    Returns:
-        Nothing
-
-    """
-    alert = QMessageBox()
-    alert.setText('You clicked Ασκήσεις!')
-    alert.exec()
-
-
-def Statistics():
-    """Event handler for the button Στατιστικά.
-
-    Args:
-        None
-
-    Returns:
-        Nothing
-
-    """
-    alert = QMessageBox()
-    alert.setText('You clicked Στατιστικά!')
-    alert.exec()
-
-def Exit():
-    """This is the event handler for the button Έξοδος
-
-    Args:
-        None
-
-    Returns:
-        Nothing
-
-    """
-    alert = QMessageBox()
-    alert.setText('You clicked Έξοδος!')
-    alert.exec()
-
-
-def FirstWindow() -> QWidget:
+class FirstWindow(QWidget) :
     """Return The First Window a user sees in the application.
 
     Args:
@@ -70,25 +12,84 @@ def FirstWindow() -> QWidget:
     Returns:
         QWidget: The QWidget that is to be shown in the QApplication
     """
-    window = QWidget()
+    def Lessons(self):
+        """Event handler for the button Μαθήματα.
 
-    layout = QVBoxLayout()
-    button = QPushButton('Μαθήματα')
-    button.clicked.connect(Lessons)
-    layout.addWidget(button)
-    button = QPushButton('Ασκήσεις')
-    button.clicked.connect(Exercises)
-    layout.addWidget(button)
-    button = QPushButton('Στατιστικά')
-    button.clicked.connect(Statistics)
-    layout.addWidget(button)
-    button = QPushButton('Έξοδος')
-    button.clicked.connect(Exit)
-    layout.addWidget(button)
+        Args:
+            None
 
-    window.setLayout(layout)
+        Returns:
+            Nothing
 
-    return window
+        """
+        
+        
+
+    def Exercises(self):
+        """Event handler for the button Ασκήσεις.
+
+        Args:
+            None
+
+        Returns:
+            Nothing
+
+        """
+        question1 = MultipleChoiceQuestion("The number is 1. What's the number?", ["2", "23", "69", "1", "23", "26"], 3)
+        question2 = MultipleChoiceQuestion("The number is 2. What's the number?", ["1", "2", "69", "2", "twenyone", "26"], 2)
+        question3 = MultipleChoiceQuestion("The number is 3. What's the number?", ["3", "twenyone", "26"], 0)
+        question4 = MultipleChoiceQuestion("The number is 4. What's the number?", ["1", "6", "4", "2", "twenyone", "26"], 2)
+
+        self.setLayout(MultipleChoiceLayout([question1, question2, question3, question4]))
+
+
+    def Statistics():
+        """Event handler for the button Στατιστικά.
+
+        Args:
+            None
+
+        Returns:
+            Nothing
+
+        """
+        alert = QMessageBox()
+        alert.setText('You clicked Στατιστικά!')
+        alert.exec()
+
+    def Exit():
+        """This is the event handler for the button Έξοδος
+
+        Args:
+            None
+
+        Returns:
+            Nothing
+
+        """
+        alert = QMessageBox()
+        alert.setText('You clicked Έξοδος!')
+        alert.exec()
+
+
+    def __init__(self):
+        super().__init__()
+    
+        layout = QVBoxLayout()
+        button = QPushButton('Μαθήματα')
+        button.clicked.connect(self.Lessons)
+        layout.addWidget(button)
+        button = QPushButton('Ασκήσεις')
+        button.clicked.connect(self.Exercises)
+        layout.addWidget(button)
+        button = QPushButton('Στατιστικά')
+        button.clicked.connect(self.Statistics)
+        layout.addWidget(button)
+        button = QPushButton('Έξοδος')
+        button.clicked.connect(self.Exit)
+        layout.addWidget(button)
+
+        self.setLayout(layout)
 
 
 def main():
