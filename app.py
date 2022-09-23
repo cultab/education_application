4f967a0cecd4c501cc9ed4779b6f673c40e5a888
cultab <rroarck@gmail.com>
2022-09-24 00:26:37 +0300

    coomit: css and style changes

--- app.py
+++ app.py
@@ -5,7 +5,10 @@ from sys import argv
 
 from PyQt5.QtWidgets import (QAction, QApplication, QMainWindow, QMenu,
                              QMenuBar, QMessageBox, QPushButton,
-                             QStackedWidget, QVBoxLayout, QWidget)
+                             QStackedWidget, QVBoxLayout, QWidget, QGraphicsDropShadowEffect)
+
+from PyQt5 import QtGui
+from PyQt5 import QtCore
 
 from Exercises import ExercisesWidget
 from Lessons import LessonsWidget
@@ -23,7 +26,11 @@ class FirstWindow(QMainWindow):
 
         layout = QVBoxLayout()
 
-        button = QPushButton('Μαθήματα')
+        lessons = QPushButton('Μαθήματα')
+        lessons.setObjectName("lessons")
+        lessons.setFlat(True)
+        lessons.setAutoFillBackground(True)
+        lessons.setProperty("cssClass", "bigButton")
 
         def openLessons():
             """TBD."""
@@ -31,28 +38,42 @@ class FirstWindow(QMainWindow):
             self.centralWidget().addWidget(viewer)
             self.centralWidget().setCurrentWidget(viewer)
 
-        button.clicked.connect(openLessons)
-        layout.addWidget(button)
+        lessons.clicked.connect(openLessons)
+        layout.addWidget(lessons)
 
-        button = QPushButton('Ασκήσεις')
+        exercises = QPushButton('Ασκήσεις')
+        exercises.setObjectName("exercises")
+        exercises.setFlat(True)
+        exercises.setAutoFillBackground(True)
+        exercises.setProperty("cssClass", "bigButton")
 
         def openExercises() -> None:
             widget = ExercisesWidget()
             self.centralWidget().addWidget(widget)
             self.centralWidget().setCurrentWidget(widget)
-        button.clicked.connect(openExercises)
+        exercises.clicked.connect(openExercises)
 
-        layout.addWidget(button)
-        button = QPushButton('Στατιστικά')
+        layout.addWidget(exercises)
+        statistics = QPushButton('Στατιστικά')
+        statistics.setObjectName("statistics")
+        statistics.setFlat(True)
+        statistics.setAutoFillBackground(True)
+        statistics.setProperty("cssClass", "bigButton")
 
         def openStatistics() -> None:
             alert = QMessageBox()
             alert.setText('You clicked Στατιστικά!')
             alert.exec()
-            button.clicked.connect(openStatistics)
-        layout.addWidget(button)
+            exercises.clicked.connect(openStatistics)
+        layout.addWidget(statistics)
 
-        button = QPushButton('Έξοδος')
+        exit_app = QPushButton('Έξοδος')
+        exit_app.setObjectName("exit")
+        exit_app.setFlat(True)
+        exit_app.setAutoFillBackground(True)
+        exit_app.setIcon(QtGui.QIcon("./resources/illustration-exit-door_53876-5844.jpg"))
+        exit_app.setIconSize(QtCore.QSize(180, 180))
+        exit_app.setProperty("cssClass", "bigButton")
 
         def exit() -> None:
             reply = QMessageBox.question(self, "QMessageBox.question()", "Σίγουρα θέλεις να τερματήσεις την εφαρμογή;", QMessageBox.Yes | QMessageBox.No | QMessageBox.Cancel)
@@ -61,8 +82,8 @@ class FirstWindow(QMainWindow):
                 self.close()
             elif reply == QMessageBox.No:
                 pass
-        button.clicked.connect(exit)
-        layout.addWidget(button)
+        exit_app.clicked.connect(exit)
+        layout.addWidget(exit_app)
 
         widget = QWidget()
         widget.setLayout(layout)
@@ -80,10 +101,44 @@ class FirstWindow(QMainWindow):
         fileMenu.addAction(exitButton)
 
 
+        # margin-right: 90%;
+        # margin-left: 90%;
+    # background-image: url(./resources/math-seamless-pattern_111409-585.jpg);
+css = """
+*[cssClass="bigButton"] {
+    font-size: 48pt;
+    color: dimgray;
+    border: 10px solid white;
+    border-radius: 30px;
+    font-weight: bold;
+    padding: 200%;
+    margin: 0px;
+}
+
+*[cssClass="bigButton"]:hover {
+        color: black;
+        border: 10px solid black;
+}
+
+QPushButton#exercises {
+    background-image: url(./resources/Hourglass_512.png);
+}
+
+QPushButton#lessons {
+    background-image: url(./resources/math-seamless_256.jpg);
+}
+
+QPushButton#statistics {
+    background-image: url(./resources/calculator_yellow.png);
+}
+
+"""
+
 
 def main():
     """Main."""
     app = QApplication(argv)
+    app.setStyleSheet(css)
     window = FirstWindow()
     window.show()
     app.exec()
