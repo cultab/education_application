70e907a40a1845861c932804710a51a3f009821a
cultab <rroarck@gmail.com>
2022-09-24 00:52:51 +0300

    docs: MenuButton docstrings

--- MenuButton.py
+++ MenuButton.py
@@ -1,10 +1,22 @@
+"""A big button that expands to fill all available space."""
 
-from PyQt5.QtWidgets import QPushButton, QSizePolicy
+from PyQt5.QtWidgets import QPushButton, QSizePolicy, QGraphicsDropShadowEffect
+
 
 class MenuButton(QPushButton):
+    """A big button that expands to fill all available space."""
 
     def __init__(self: QPushButton, text: str, object_name: str = None) -> None:
+        """Initialize a MenuButton.
+
+        text: the button's label
+        object_name: (optional) an object name
+        """
         super(MenuButton, self).__init__(text)
+
+        # shadow = QGraphicsDropShadowEffect()
+        # shadow.setBlurRadius(15)
+        # self.setGraphicsEffect(shadow)
         if object_name:
             self.setObjectName(object_name)
         self.setFlat(True)
